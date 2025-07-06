fd6082de3bd1154287c00e4cdd4187e1f3042fef
clandestino161 <clandestino161@pm.me>
2025-07-06 14:15:54 +0200

    add verbose mode

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
 
 client = genai.Client(api_key=api_key)
 
@@ -13,13 +13,19 @@ def main():
     if len(sys.argv) < 2:
         print("No prompt provided")
         exit(1)
-    else:
-        prompt = sys.argv[1]
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
-        print(response.text)
+    elif "--verbose" in sys.argv:
+        user_prompt = " ".join([arg for arg in sys.argv[1:] if arg != "--verbose"])
+        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+        print(response.text)
+    else:
+        user_prompt = " ".join([arg for arg in sys.argv[1:] if arg != "--verbose"])
+        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+        print(response.text)
 
 if __name__ == "__main__":
     main()
